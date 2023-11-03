70e5eaac776bc168ebaff362bcf73f41a39f1b44
Miclyael <chardionluge@gmail.com>
2023-11-04 06:37:44 +0700

    Near-Finished Apps, Addded Database Encryption, Obfuscation Added, Certificate Pinning Added

--- core/src/main/java/com/latihan_android/core/di/CoreModule.kt
+++ core/src/main/java/com/latihan_android/core/di/CoreModule.kt
@@ -10,6 +10,7 @@ import com.latihan_android.core.domain.repository.IUserRepository
 import com.latihan_android.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
@@ -59,10 +60,19 @@ val authInterceptor = Interceptor { chain ->
 
 val networkModule = module {
     single {
+        val hostname="api.github.com"
+        val certificatePinner= CertificatePinner.Builder()
+            .add(hostname,"sha256/jFaeVpA8UQuidlJkkpIdq3MPwD0m8XbuCRbJlezysBE=")
+            .add(hostname,"sha256/Wec45nQiFwKvHtuHxSAMGkt19k+uPSw9JlEkxhvYPHk=")
+            .add(hostname,"sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+            .add(hostname,"sha256/e0IRz5Tio3GA1Xs4fUVWmH1xHDiH2dMbVtCBSkOIdqM=")
+
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(authInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
 
     }
