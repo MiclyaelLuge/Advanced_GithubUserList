f1ed7160d38dac2a14cd4233677cbf78ae2b430b
Miclyael <chardionluge@gmail.com>
2023-11-04 05:33:20 +0700

    Near-Finished Apps, Addded Database Encryption, Obfuscation Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,15 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
